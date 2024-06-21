88bc1f3fb2f64a540ec7c378a39d9f4c153a62f1
LittleDijkstraZ <1944618985@qq.com>
2024-06-21 21:38:32 +0800

    updated utils

--- code.py
+++ code.py
@@ -1,1 +1,6 @@
-print('done')
+
+try:
+    sum_of_squares = distance**2 + (sqrt(k*solutions[0]**2 - 2*k*solutions[0] + l)**2 + (sqrt(k*solutions[1]**2 - 2*k*solutions[1] + l)**2)
+except Exception as e:
+    print(e)
+print(a)
